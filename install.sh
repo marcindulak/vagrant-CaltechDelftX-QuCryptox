e1e20ae370be9170d74281595a83b2cb48f3b739
Marcin Dulak <marcin.dulak@gmail.com>
2017-02-18 15:36:09 +0100

    new epel

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # skip if already installed
 if test -d /root/.julia; then exit 0; fi
 yum clean all
-if `cat /etc/redhat-release | grep -qi 'release 7'` && ! rpm -q epel-release; then yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm; fi
+if `cat /etc/redhat-release | grep -qi 'release 7'` && ! rpm -q epel-release; then yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm; fi
 wget -q https://copr.fedorainfracloud.org/coprs/nalimilan/julia/repo/epel-7/nalimilan-julia-epel-7.repo -O /etc/yum.repos.d/nalimilan-julia-epel-7.repo
 yum -y install julia
 # c++ needed by ZMQ build
